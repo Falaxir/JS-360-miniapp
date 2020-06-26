3c01420f918f2d46c4d71a8ec62f7fec871fd27f
Falaxir <falaxir@gmail.com>
2020-06-26 13:09:26 +0200

    Coorrection tableau JSON Les données sont bien a partir d'un JSON Correction barre de recherche Ajout du CSS de la page des produits Netoyage du code

--- src/base/app.js
+++ src/base/app.js
@@ -1,61 +1,4 @@
-var test = [{
-    "name": "sildenafil citrate",
-    "supplier": "Wisozk Inc",
-    "quantity": 261,
-    "unit_cost": "$10.47"
-  }, {
-    "name": "chinel",
-    "supplier": "Wisozk Inc",
-    "quantity": 261,
-    "unit_cost": "$10.47"
-  }, {
-    "name": "Dextromathorphan HBr",
-    "supplier": "Schmitt-Weissnat",
-    "quantity": 211,
-    "unit_cost": "$20.53"
-  }, {
-    "name": "Dextromathorphan HBr",
-    "supplier": "Schmitt-Weissnat",
-    "quantity": 199,
-    "unit_cost": "$209.53"
-  }]
-
-function contenttablefct(data) {
-    contenttable = data
-    console.log(contenttable)
-    contenttablefct.content = data
-    return contenttable
-}
-
-function get_file(file, type) {
-    var xhr = new XMLHttpRequest()
-    var contenttable = []
-
-    xhr.open("GET", file, true)
-    xhr.responseType = type
-    xhr.onload = function() {
-        switch (this.status) {
-            case 200:
-                //resultatdisp.innerHTML = JSON.stringify(this.response);
-                var data = xhr.response
-                contenttable = data
-                contenttablefct(data)
-                break
-            case 404:
-                alert("Erreur de Chargement de la page")
-                break
-            default:
-                break
-        }
-    }
-    xhr.send()
-    return contenttable
-}
-
-//get_file("list.json", "json")
-
-var test52 = get_file("list.json", "json")
-console.log(test52)
+var backup_table_list = []
 
 var Table_Lignes = new Vue({
     el: "#tablelines",
@@ -64,14 +7,35 @@ var Table_Lignes = new Vue({
     }
 })
 
+function refresh_backuptable(data) {
+    backup_table_list = data.slice()
+}
+
+function search_name(tofindht) {
+    var tablenum = 0
+
+    for (let i = 0; i < backup_table_list.length; i++) {
+        Table_Content.product.pop()
+    }
+    for (let i = 0; i < backup_table_list.length; i++) {
+        if (backup_table_list[i].name.includes(tofindht) == true) {
+            Table_Content.product[tablenum] = backup_table_list[i]
+            tablenum++
+        }
+    }
+}
+
 var Table_Content = new Vue({
     el: "#table_content",
-    mounted: function () {
-        contenttable = get_file("list.json", "json")
-    },
-    colun: 4,
     data: {
-        product: test
+        product: []
+    },
+    mounted() {
+        var self = this
+        $.getJSON("list.json", function(data) {
+          self.product = data
+          refresh_backuptable(data)
+        })
     }
 })
 
@@ -87,67 +51,3 @@ var Table_Title = new Vue({
         }]
     }
 })
-
-/*
-
-Notes for JSON importation, don't know why it isn't working properly
-
-*/
-
-/*function filtres() {
-    const filter = document.getElementById("Filtres");
-
-    filter.addEventListener("submit", function(req) {
-        req.preventDefault()
-        const request = new XMLHttpRequest()
-
-        const data = new FormData(filter)
-        request.open("post", "getelement.php", true)
-        //request.onload = function () {
-        //    console.log("YES")
-        //    console.log(request.responseText)
-        //}
-        request.send(data)
-    })
-}*/
-
-/*document.getElementById("Filtres").addEventListener("submit", function(request) {
-    request.preventDefault()
-    var resultatdisp = document.getElementById("resultat")
-    var formidable = document.getElementById("Filtres")
-    var data = new FormData(formidable)
-    var xhr = new XMLHttpRequest()
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            //resultatdisp.innerHTML = JSON.stringify(this.response)
-            console.log(this.response)
-        } else if (this.readyState == 4 && this.status == 404) {
-            alert("Error")
-        }
-    }
-
-    xhr.open("POST", "getelement.php", true)
-    //xhr.responseType = "json"
-    xhr.send(data)
-});*/
-
-/*document.getElementById("Filtres").addEventListener("submit", function(request) {
-    request.preventDefault()
-    var data = new FormData(this)
-    var xhr = new XMLHttpRequest()
-    var resultatdisp = document.getElementById("resultat")
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            resultatdisp.innerHTML = JSON.stringify(this.response)
-            console.log(this.response)
-        } else if (this.readyState == 4 && this.status == 404) {
-            alert("Error")
-        }
-    }
-
-    xhr.open("GET", "list.json", true)
-    xhr.responseType = "json"
-    xhr.send(data)
-});*/
--- src/filter.js
+++ src/filter.js
@@ -1,23 +1,3 @@
-/*
-idee : pour vuejs JSON faire comme backup table list, slice le tableau et le remplacer
-*/
-
-var backup_table_list = Table_Content.product.slice();
-
-function search_name(tofindht) {
-    var tablenum = 0
-
-    for (let i = 0; i < backup_table_list.length; i++) {
-        Table_Content.product.pop()
-    }
-    for (let i = 0; i < backup_table_list.length; i++) {
-        if (backup_table_list[i].name.includes(tofindht) == true) {
-            Table_Content.product[tablenum] = backup_table_list[i]
-            tablenum++
-        }
-    }
-}
-
 function has_already_been_sorted(picked) {
     if (picked == 2) {
         for (let i = 0; i < Table_Content.product.length-1; i++) {
@@ -34,8 +14,8 @@ function has_already_been_sorted(picked) {
     return 84
 }
 
+/* 1 = croissant 2 = decroissant */
 function pick_price_sort(picked) {
-    // 1 = croissant 2 = decroissant
     console.log(picked)
     if ((picked != 1 && picked != 2) || has_already_been_sorted(picked) == 84)
         return (84)
@@ -49,9 +29,3 @@ function pick_name(value) {
     console.log(Table_Content.product)
     window.location = "product.html" + "?name=" + value.name + "&supplier=" + value.supplier + "&quantity=" + value.quantity + "&unit_cost=" + value.unit_cost
 }
-
-/*
-    "supplier": "Schmitt-Weissnat",
-    "quantity": 211,
-    "unit_cost": "$20.53"
-*/
